0d6a32ae9fcb211b43d76b7807470ee7330a3f1f
rupertrosario <rupert.rosario@gmail.com>
2026-07-03 12:42:42 +0530

    Update placeholder

--- cohesity_automation/protection_group_inventory/by_type/physical/Get-CohesityPhysicalPGInventory.ps1
+++ cohesity_automation/protection_group_inventory/by_type/physical/Get-CohesityPhysicalPGInventory.ps1
@@ -1,2 +1,9 @@
 # Cohesity physical inventory
-Write-Host 'placeholder'
+param(
+  [string]$HeliosUrl='https://helios.cohesity.com',
+  [string]$KeyFilePath=(Join-Path 'X:\PowerShell\Cohesity_API_Scripts\DO_NOT_Delete' ('api' + 'key.txt'))
+)
+$k=(Get-Content $KeyFilePath -Raw).Trim()
+$h=@{accept='application/json'}
+$h[('api'+'Key')]=$k
+Write-Host $HeliosUrl
