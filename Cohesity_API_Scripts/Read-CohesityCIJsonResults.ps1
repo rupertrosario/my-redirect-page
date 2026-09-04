57deb2544c8686bb89eab76f4311600c0ad290c8
rupertrosario <rupert.rosario@gmail.com>
2026-09-04 17:08:56 +0530

    Improve CI JSON results table output

--- Cohesity_API_Scripts/Read-CohesityCIJsonResults.ps1
+++ Cohesity_API_Scripts/Read-CohesityCIJsonResults.ps1
@@ -3,7 +3,7 @@ param(
     [string]$Path
 )
 
-# Read a Cohesity Siren / CI diagnostic JSON file and present .results in a human-readable table.
+# Read Cohesity Siren / CI diagnostic JSON and show only operationally useful fields.
 $json = Get-Content -Path $Path -Raw | ConvertFrom-Json
 
 if (-not $json.results) {
@@ -12,21 +12,29 @@ if (-not $json.results) {
 }
 
 $json.results |
-    Select-Object testNumber,
-                  testName,
-                  state,
-                  severity,
-                  module,
-                  code,
-                  @{Name='Details'; Expression={
-                      if ($null -eq $_.data) {
-                          ''
-                      }
-                      elseif ($_.data -is [string]) {
-                          $_.data
-                      }
-                      else {
-                          $_.data | ConvertTo-Json -Depth 10 -Compress
-                      }
-                  }} |
+    Select-Object `
+        @{Name='Test #'; Expression={$_.testNumber}},
+        @{Name='Test Name'; Expression={$_.testName}},
+        @{Name='State'; Expression={$_.state}},
+        @{Name='Severity'; Expression={$_.severity}},
+        @{Name='Module'; Expression={$_.module}},
+        @{Name='Code'; Expression={$_.code}},
+        @{Name='Result'; Expression={
+            if ($null -ne $_.out -and "$($_.out)".Trim()) {
+                if ($_.out -is [string]) { $_.out }
+                else { $_.out | ConvertTo-Json -Depth 10 -Compress }
+            }
+            elseif ($null -ne $_.data) {
+                if ($_.data -is [string]) { $_.data }
+                else { $_.data | ConvertTo-Json -Depth 10 -Compress }
+            }
+            else {
+                ''
+            }
+        }},
+        @{Name='Remediation'; Expression={
+            if ($null -eq $_.remediation) { '' }
+            elseif ($_.remediation -is [string]) { $_.remediation }
+            else { $_.remediation | ConvertTo-Json -Depth 10 -Compress }
+        }} |
     Format-Table -AutoSize -Wrap
