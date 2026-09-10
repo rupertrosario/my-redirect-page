c54dc2fbfcdcfdf21bdf2a9d7809076fed90deb8
rupertrosario <rupert.rosario@gmail.com>
2026-09-10 16:15:16 +0530

    Include CR number in requested backup group filter output

--- dtsk_backup_status/12_cr_filter_requested_backup_group.js
+++ dtsk_backup_status/12_cr_filter_requested_backup_group.js
@@ -1,16 +1,21 @@
 import { result } from "@dynatrace-sdk/automation-utils";
 
 export default async function () {
+  const crs = await result("get_crs");
   const groups = await result("get_requested_groups");
   const BACKUP_GROUP_SYS_ID = "PUT_BACKUP_GROUP_SYS_ID_HERE";
 
   const matches = [];
 
-  for (const groupList of groups || []) {
-    for (const row of groupList || []) {
+  for (let i = 0; i < (groups || []).length; i++) {
+    const groupList = groups[i] || [];
+    const cr = crs?.[i] || {};
+
+    for (const row of groupList) {
       if (row?.assignment_group?.value === BACKUP_GROUP_SYS_ID) {
         matches.push({
           crSysId: row?.parent?.value || "",
+          crNumber: cr?.number?.value || cr?.number || "",
           groupSysId: row?.assignment_group?.value || ""
         });
       }
